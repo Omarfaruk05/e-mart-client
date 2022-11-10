daf182edd13d1e68f7d3e117865fe486770e3fc0
Omarfaruk05 <mdomarfrauk149518.com>
2022-11-11 00:51:03 +0600

    singup complete

--- src/pages/LoginRegistration/LoginRegistration.js
+++ src/pages/LoginRegistration/LoginRegistration.js
@@ -4,6 +4,7 @@ import loginPhoto from '../../assets/login-ragistration/login.jpg'
 import singUpPhoto from '../../assets/login-ragistration/sing-up.jpg'
 
 const LoginRegistration = () => {
+    const [user, setUser] = useState({});
     const [login, setLogin] = useState(true);
     const [signUp, setSignUp] = useState(false);
 
@@ -17,8 +18,45 @@ const LoginRegistration = () => {
         setSignUp(true);
     };
 
+    const handleSignupSubmit = (e) => {
+        e.preventDefault();
+        const firstName = e.target.firstName.value;
+        const lastName = e.target.lastName.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const confirmPassword = e.target.confirmPassword.value;
+        const user = {firstName, lastName, email, password, confirmPassword};
+
+        const url="http://localhost:5000/api/v1/user/signup"
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res=> res.json())
+        .then(data=> {
+            if(data.status === "Success"){
+                console.log(data.data)
+                setUser(data.data);
+                e.target.reset();
+
+            }
+            if(data.status === "Fail"){
+                console.log(data.error);
+            }
+        });
+
+
+        
+    }
+
+    console.log(user);
+
     return (
-        <div>
+        <div className={user._id ?'hidden': "block"}>
             <input type="checkbox" id="login-registration-modal" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box max-w-4xl rounded-md m-0 p-0 relative">
@@ -35,8 +73,8 @@ const LoginRegistration = () => {
                                 <div>
                                     <form>
                                         <div>
-                                            <input className='input input-bordered rounded-sm w-full mb-4' type="email" name="email" id="" placeholder='Your Email: *' required />
-                                            <input className='input input-bordered rounded-sm w-full mb-4' type="password" name="password" id="" placeholder='Password: *' required />
+                                            <input className='input input-bordered rounded-sm w-full mb-4' type="email" name="email" placeholder='Your Email: *' required />
+                                            <input className='input input-bordered rounded-sm w-full mb-4' type="password" name="password" placeholder='Password: *' required />
                                             <input className='btn btn-primary w-full rounded-sm font-bold' type="submit" value="Login" />
                                         </div>
                                     </form>
@@ -54,15 +92,15 @@ const LoginRegistration = () => {
                             </div>
                             <div className='flex items-center px-4 md:w-1/2 h-[500px] text-gray-700 bg-slate-200'>
                                 <div>
-                                    <form>
+                                    <form onSubmit={handleSignupSubmit}>
                                         <div>
                                             <div className='flex gap-2'>
-                                                <input className='input input-bordered rounded-sm w-full mb-4' type="text" name="firstName" id="" placeholder='First Name: *' required />
-                                                <input className='input input-bordered rounded-sm w-full mb-4' type="text" name="lastName" id="" placeholder='Last Name: *' required />
+                                                <input className='input input-bordered rounded-sm w-full mb-4' type="text" name="firstName" placeholder='First Name: *' required />
+                                                <input className='input input-bordered rounded-sm w-full mb-4' type="text" name="lastName" placeholder='Last Name: *' required />
                                             </div>
-                                            <input className='input input-bordered rounded-sm w-full mb-4' type="email" name="email" id="" placeholder='Your Email: *' required />
-                                            <input className='input input-bordered rounded-sm w-full mb-4' type="password" name="password" id="" placeholder='Password: *' required />
-                                            <input className='input input-bordered rounded-sm w-full mb-4' type="password" name="confirmPassword" id="" placeholder='Confirm Password: *' required />
+                                            <input className='input input-bordered rounded-sm w-full mb-4' type="email" name="email" placeholder='Your Email: *' required />
+                                            <input className='input input-bordered rounded-sm w-full mb-4' type="password" name="password" placeholder='Password: *' required />
+                                            <input className='input input-bordered rounded-sm w-full mb-4' type="password" name="confirmPassword" placeholder='Confirm Password: *' required />
                                             <input className='btn btn-primary w-full rounded-sm font-bold' type="submit" value="Sing Up" />
                                         </div>
                                     </form>
